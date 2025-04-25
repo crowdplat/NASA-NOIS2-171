0cf1d00dd59405e88db714703b090f55a8d8d9a0
Sadman Sakib <sadman.93.sakib@gmail.com>
2025-04-24 20:09:13 -0400

    add validation checks and better log messages

--- crispv2_1/image_scripts/preprocess_images.py
+++ crispv2_1/image_scripts/preprocess_images.py
@@ -5,9 +5,7 @@ import os
 import argparse
 import json
 import albumentations as A
-import matplotlib.pyplot as plt
 import pandas as pd
-
 Image.MAX_IMAGE_PIXELS = None
 
 ### Sample Command to pre-process image dataset ###
@@ -42,73 +40,72 @@ class Transformations:
         image_folder = self.preprocess_config.get("image_folder", "img_input")
         output_root_folder = self.preprocess_config.get("preprocessed_output_folder", f"{image_folder}_processed")
         environments = self.preprocess_config.get("environments", "env_split")
-        
-        original_resized_folder = os.path.join(output_root_folder, "original_resized")
-        os.makedirs(original_resized_folder, exist_ok=True)
-        
 
-        saved_images_df = pd.DataFrame(columns=['image_name', environments])
-        saved_image_names, saved_env_split = list(), list()
+        # If input directory exists then proceed with transformation steps!
+        if os.path.isdir(image_folder):
+            original_resized_folder = os.path.join(output_root_folder, "original_resized")
+            os.makedirs(original_resized_folder, exist_ok=True)
 
-        for filename in os.listdir(image_folder):
-            if filename.endswith(('.jpg', '.jpeg', '.png')):
-                file_path = os.path.join(image_folder, filename)
-                
-                try:
-                    with Image.open(file_path) as img:
-                        img_gray = img.convert("L")
-                        img_array_gray = np.array(img_gray)
+            saved_images_df = pd.DataFrame(columns=['image_name', environments])
+            saved_image_names, saved_env_split = list(), list()
 
-                        clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8)) # ADAPTIVE HE
-                        equalized_img_array = clahe.apply(img_array_gray)
-
-                        equalized_img = Image.fromarray(equalized_img_array)
-
-                        intermediate_size = (equalized_img.size[0] // 4, equalized_img.size[1] // 4)
-                        img_intermediate = equalized_img.resize(intermediate_size, Image.Resampling.NEAREST)
-                        img_resized = img_intermediate.resize(self.image_size, Image.Resampling.NEAREST)
-
-                        img_array = np.array(img_resized) / 255.0
-
-                        output_image_path = os.path.join(original_resized_folder, f"{os.path.splitext(filename)[0]}.npy")
-                        print('Saving', output_image_path)
-                        np.save(output_image_path, img_array)
-                        saved_image_names.append(os.path.join("original_resized", f"{os.path.splitext(filename)[0]}.npy"))
-                        saved_env_split.append("original_resized")
-
-                        # plt.imshow(img_array)
-                        # plt.title(f"{os.path.splitext(filename)[0]}.npy")
-                        # plt.show()
+            all_files = os.listdir(image_folder)
+            # Check if folder empty or not
+            if(len(all_files)>0):
+                for filename in all_files:
+                    if filename.endswith(('.jpg', '.jpeg', '.png')):
+                        print("Applying Transformations to images . . .")
+                        file_path = os.path.join(image_folder, filename)
                         
-                        # Apply additional transformations on original pre-processed images
-                        for transform_name in self.transforms.keys():
-                            transformed_img = self.apply_transform(np.array(img_resized), transform_name)
-                            transformed_img_resized = cv2.resize(transformed_img, self.image_size)
-                            transformed_img_array = transformed_img_resized / 255.0
-                            
-                            transform_folder = os.path.join(output_root_folder, transform_name)
-                            os.makedirs(transform_folder, exist_ok=True)
-                            
-                            output_image_path = os.path.join(transform_folder, f"{os.path.splitext(filename)[0]}.npy")
-                            np.save(output_image_path, transformed_img_array)
-                            print(f"Saved: {output_image_path}")
+                        try:
+                            with Image.open(file_path) as img:
+                                img_gray = img.convert("L")
+                                img_array_gray = np.array(img_gray)
 
-                            saved_image_names.append(os.path.join(transform_name, f"{os.path.splitext(filename)[0]}.npy"))
-                            saved_env_split.append(transform_name)
+                                clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8)) # ADAPTIVE HE
+                                equalized_img_array = clahe.apply(img_array_gray)
+                                equalized_img = Image.fromarray(equalized_img_array)
 
-                            # plt.imshow(transformed_img_array)
-                            # plt.title(f"{os.path.splitext(filename)[0]}.npy")
-                            # plt.show()
-                
-                except Exception as e:
-                    print(f"Error processing {filename}: {e}")
+                                intermediate_size = (equalized_img.size[0] // 4, equalized_img.size[1] // 4)
+                                img_intermediate = equalized_img.resize(intermediate_size, Image.Resampling.NEAREST)
+                                img_resized = img_intermediate.resize(self.image_size, Image.Resampling.NEAREST)
+                                img_array = np.array(img_resized) / 255.0
 
-        saved_images_df['image_name']=saved_image_names
-        saved_images_df[environments]=saved_env_split
-        image_names_save_path = os.path.join(output_root_folder, 'labels.csv')
-        saved_images_df.sort_values(by=[environments, 'image_name'], ascending=[True, True], inplace=True)
-        print('Saving image names csv', image_names_save_path)
-        saved_images_df.to_csv(image_names_save_path, index=False)
+                                output_image_path = os.path.join(original_resized_folder, f"{os.path.splitext(filename)[0]}.npy")
+                                print('Saving', output_image_path)
+                                np.save(output_image_path, img_array)
+                                saved_image_names.append(os.path.join("original_resized", f"{os.path.splitext(filename)[0]}.npy"))
+                                saved_env_split.append("original_resized")
+                                
+                                # Apply additional transformations on original pre-processed images
+                                for transform_name in self.transforms.keys():
+                                    transformed_img = self.apply_transform(np.array(img_resized), transform_name)
+                                    transformed_img_resized = cv2.resize(transformed_img, self.image_size)
+                                    transformed_img_array = transformed_img_resized / 255.0
+                                    
+                                    transform_folder = os.path.join(output_root_folder, transform_name)
+                                    os.makedirs(transform_folder, exist_ok=True)
+                                    
+                                    output_image_path = os.path.join(transform_folder, f"{os.path.splitext(filename)[0]}.npy")
+                                    np.save(output_image_path, transformed_img_array)
+                                    print(f"Saved: {output_image_path}")
+
+                                    saved_image_names.append(os.path.join(transform_name, f"{os.path.splitext(filename)[0]}.npy"))
+                                    saved_env_split.append(transform_name)
+                        
+                        except Exception as e:
+                            print(f"Error processing {filename}: {e}")
+
+                saved_images_df['image_name']=saved_image_names
+                saved_images_df[environments]=saved_env_split
+                image_names_save_path = os.path.join(output_root_folder, 'labels.csv')
+                saved_images_df.sort_values(by=[environments, 'image_name'], ascending=[True, True], inplace=True)
+                print('Saving image names csv', image_names_save_path)
+                saved_images_df.to_csv(image_names_save_path, index=False)
+            else:
+                print(f'Warning: Directory {image_folder} is empty! Please input a valid directory with image files (.jpg or .jpeg or .png).')
+        else:
+            print(f'Warning: Directory {image_folder} is invalid! Please input a valid directory.')
     
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='CRISP Image Preprocessing',
@@ -122,5 +119,4 @@ if __name__ == '__main__':
         preprocess_config = json.load(json_file)
 
     transformations = Transformations(preprocess_config)
-    print("Applying Transformations to images . . .")
     transformations.transform_images()
